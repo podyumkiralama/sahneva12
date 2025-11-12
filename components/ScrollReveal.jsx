e555c8e8fbe8eb5c431f0d5952126bc0353911cd
Murat <kasimrize@gmail.com>
2025-11-13 02:31:25 +0300

    Update ScrollReveal.jsx

--- components/ScrollReveal.jsx
+++ components/ScrollReveal.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useScrollAnimation } from '@/hooks/useScrollAnimation';
+import { useScrollAnimation } from '@/compenents/hooks/useScrollAnimation';
 
 export function ScrollReveal({ 
   children, 
