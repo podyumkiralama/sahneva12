f622d4622b16acd477508cc00dccdf7411e3505c
Murat <kasimrize@gmail.com>
2025-11-13 02:27:07 +0300

    Create ScrollReveal.jsx

---
+++ components/ScrollReveal.jsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useScrollAnimation } from '@/hooks/useScrollAnimation';
+
+export function ScrollReveal({ 
+  children, 
+  className = '', 
+  delay = '',
+  direction = 'up',
+  ...props 
+}) {
+  const ref = useScrollAnimation();
+  
+  const getAnimationClass = () => {
+    switch (direction) {
+      case 'left': return 'reveal-left';
+      case 'right': return 'reveal-right';
+      case 'scale': return 'reveal-scale';
+      default: return 'reveal';
+    }
+  };
+
+  return (
+    <div
+      ref={ref}
+      className={`${getAnimationClass()} ${delay ? `delay-${delay}` : ''} ${className}`}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+}
+
+// Grup halinde animasyon için bileşen
+export function ScrollRevealGroup({ 
+  children, 
+  className = '',
+  staggerDelay = 2 // 0.2 saniye aralıklarla
+}) {
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  );
+}
