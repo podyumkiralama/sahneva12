2d75afbaef1ff78d325a4c5e0438dff1184c29fb
Murat <kasimrize@gmail.com>
2025-11-15 22:40:41 +0300

    Update page.js

--- app/(tr)/masa-sandalye-kiralama/page.js
+++ app/(tr)/masa-sandalye-kiralama/page.js
@@ -1,7 +1,6 @@
 // app/masa-sandalye-kiralama/page.jsx
 import Image from "next/image";
 import Link from "next/link";
-import Script from "next/script";
 import dynamic from "next/dynamic";
 
 import { buildFaqSchema } from "@/lib/structuredData/faq";
