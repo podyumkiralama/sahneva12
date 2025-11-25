3366834534bf481df3b342ad6dc1253ba9045131
Murat <kasimrize@gmail.com>
2025-11-25 20:19:26 +0300

    Update layout.js

--- app/(tr)/layout.js
+++ app/(tr)/layout.js
@@ -1,32 +1,27 @@
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import UtilityBar from "../../components/UtilityBar.client";
-import StickyVideoRail from "@/components/StickyVideoRail"; // Bunu Root'tan buraya taşıdım
+import UtilityBar from "../../components/UtilityBar.client"; 
+import StickyVideoRail from "@/components/StickyVideoRail";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function TurkishLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* Skip Link erişilebilirlik için header'dan önce gelebilir veya Root'ta kalabilir, 
-          ama görsel akış burada başlıyor. */}
-          
-      {/* Video Rail ve Utility Bar buraya alındı */}
+    <div className="flex flex-col min-h-screen relative">
+      
       <StickyVideoRail />
       
       <header
-        id="_main_header"
-        tabIndex={-1}
-        aria-label="Sahneva ana site başlığı"
-        className="w-full"
+        id="main-header"
+        className="w-full relative z-40"
       >
         <UtilityBar />
         <Navbar />
       </header>
 
       <main
-        id="_main_content"
+        id="main-content"
+        className="flex-1 pt-0 focus:outline-none scroll-mt-24" 
         tabIndex={-1}
-        className="flex-1 pt-6 pb-10 lg:pb-12 focus-ring scroll-mt-4"
       >
         {children}
       </main>
