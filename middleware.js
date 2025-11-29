351d4bdeb1fb71d196cef36c97a3f928af9c4679
Murat <kasimrize@gmail.com>
2025-11-30 00:31:44 +0300

    Remove HTML rewriting from middleware

--- middleware.js
+++ middleware.js
@@ -1,90 +1,23 @@
 import { NextResponse } from "next/server";
 
-const EdgeHTMLRewriter = globalThis.HTMLRewriter;
-
-const VOID_TAGS = [
-  "meta",
-  "link",
-  "img",
-  "input",
-  "br",
-  "hr",
-  "area",
-  "base",
-  "col",
-  "embed",
-  "param",
-  "source",
-  "track",
-  "wbr",
-];
-
-const HTML_ACCEPT_HEADER = "text/html";
-
-class VoidElementFormatter {
-  element(element) {
-    const tagName = element.tagName?.toLowerCase();
-
-    if (!tagName) {
-      return;
-    }
-
-    const attributes = [];
-
-    for (const [name, rawValue] of element.attributes) {
-      const attributeName = name.toLowerCase();
-      if (rawValue == null) {
-        attributes.push(attributeName);
-        continue;
-      }
-
-      const value = String(rawValue);
-
-      if (value === "") {
-        attributes.push(`${attributeName}=""`);
-        continue;
-      }
-
-      attributes.push(`${attributeName}="${escapeAttribute(value)}"`);
-    }
-
-    const serialized = `<${tagName}${attributes.length ? " " + attributes.join(" ") : ""}>`;
-    element.replace(serialized, { html: true });
-  }
-}
-
-function escapeAttribute(value) {
-  return value
-    .replace(/&/g, "&amp;")
-    .replace(/"/g, "&quot;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/'/g, "&#39;");
-}
+const SUPPORTED_LOCALES = ["en", "ar", "tr"];
 
 export const config = {
   matcher: ["/:path*"],
 };
 
 export default function middleware(request) {
-  const accept = request.headers.get("accept");
+  const requestHeaders = new Headers(request.headers);
 
-  if (!accept || !accept.includes(HTML_ACCEPT_HEADER)) {
-    return NextResponse.next();
-  }
+  const pathname = request.nextUrl.pathname || "/";
+  const firstSegment = pathname.split("/")[1]?.toLowerCase();
+  const locale = SUPPORTED_LOCALES.includes(firstSegment) ? firstSegment : "tr";
+  const direction = locale === "ar" ? "rtl" : "ltr";
 
-  if (!EdgeHTMLRewriter) {
-    return NextResponse.next();
-  }
+  requestHeaders.set("x-locale", locale);
+  requestHeaders.set("x-direction", direction);
 
-  const response = NextResponse.next();
-  const rewriter = new EdgeHTMLRewriter();
-
-  for (const tag of VOID_TAGS) {
-    rewriter.on(tag, new VoidElementFormatter());
-  }
-
-  const transformed = rewriter.transform(response);
-  transformed.headers.set("content-type", "text/html; charset=utf-8");
-  return transformed;
+  return NextResponse.next({
+    request: { headers: requestHeaders },
+  });
 }
