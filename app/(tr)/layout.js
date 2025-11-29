eb1761779dbf8052bc0c7a37990b0862cb3e5cc1
Murat <kasimrize@gmail.com>
2025-11-29 03:23:22 +0300

    Update layout.js

--- app/(tr)/layout.js
+++ app/(tr)/layout.js
@@ -1,3 +1,4 @@
+// app/layout.jsx
 import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
@@ -6,27 +7,35 @@ export default function TurkishLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen bg-white text-neutral-900">
       
-      {/* ---- HEADER ---- */}
+      {/* ---- HEADER ---- 
+         Navbar 'fixed' olduğu için header container'ı sadece semantik amaçlıdır.
+         tabIndex kaldırıldı, header'a focuslanmak standart bir davranış değildir.
+      */}
       <header
         id="_main_header"
         aria-label="Sahneva ana site başlığı"
-        className="w-full"
-        tabIndex={-1}
+        className="w-full relative z-50" 
       >
         <Navbar />
       </header>
 
-      {/* ---- MAIN CONTENT ---- */}
+      {/* ---- MAIN CONTENT ---- 
+         DÜZELTME: Navbar fixed olduğu için içeriğin altında kalmaması adına
+         padding-top (pt) değerleri Navbar yüksekliği ile eşitlendi.
+         Mobil: h-16 -> pt-16
+         Desktop: h-20 -> lg:pt-20
+         Ekstra boşluk için +4 veya +6 eklenebilir.
+      */}
       <main
         id="_main_content"
-        className="flex-1 pt-6 pb-10 lg:pb-12 focus-ring scroll-mt-4"
+        className="flex-1 pt-16 lg:pt-20 focus:outline-none scroll-mt-24"
         tabIndex={-1}
       >
         {children}
       </main>
 
       {/* ---- FOOTER ---- */}
-      <div id="site-footer" className="w-full mt-auto">
+      <div id="site-footer-wrapper" className="w-full mt-auto">
         <Footer />
       </div>
 
