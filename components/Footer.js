a55fed22caf57e7bed726781b29cd67294743c13
Murat <kasimrize@gmail.com>
2025-12-01 04:37:35 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -109,7 +109,7 @@ export default function Footer() {
   };
 
   return (
-    <div id="site-footer-wrapper" className="w-full mt-auto">
+    <div id="site-footer-wrapper" className="w-full">
       <footer
         id="_main_footer"
         role="contentinfo"
