04b85482a1530df54db83417c25989f66637f51d
Murat <kasimrize@gmail.com>
2025-12-02 23:38:32 +0300

    Update ScrollReveal.jsx

--- components/ScrollReveal.jsx
+++ components/ScrollReveal.jsx
@@ -1,70 +1,7 @@
-"use client";
-
-import React, { forwardRef } from "react";
-
-// Basit class merge helper
-function mergeClassNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+export function ScrollReveal({ children }) {
+  return children;
 }
 
-/**
- * ScrollReveal – NO-OP, ama asChild destekli
- * Hiç animasyon yapmıyor, sadece DOM yapısını bozmadan wrapper görevi görüyor.
- */
-const ScrollRevealBase = (
-  {
-    as,          // örn: "section"
-    asChild,     // true ise child'ı root olarak kullan
-    children,
-    className = "",
-    ...rest
-  },
-  ref
-) => {
-  // asChild: <ScrollReveal asChild><li /></ScrollReveal>
-  if (asChild && React.isValidElement(children)) {
-    return React.cloneElement(children, {
-      ref,
-      className: mergeClassNames(children.props.className, className),
-      ...rest,
-    });
-  }
-
-  const Component = as || "div";
-
-  return (
-    <Component ref={ref} className={className} {...rest}>
-      {children}
-    </Component>
-  );
-};
-
-export const ScrollReveal = forwardRef(ScrollRevealBase);
-
-/**
- * ScrollRevealGroup – sadece layout wrapper'ı, animasyon yok
- */
-const ScrollRevealGroupBase = (
-  { as, asChild, children, className = "", ...rest },
-  ref
-) => {
-  if (asChild && React.isValidElement(children)) {
-    return React.cloneElement(children, {
-      ref,
-      className: mergeClassNames(children.props.className, className),
-      ...rest,
-    });
-  }
-
-  const Component = as || "div";
-
-  return (
-    <Component ref={ref} className={className} {...rest}>
-      {children}
-    </Component>
-  );
-};
-
-export const ScrollRevealGroup = forwardRef(ScrollRevealGroupBase);
-
-export default ScrollReveal;
+export function ScrollRevealGroup({ children }) {
+  return children;
+}
