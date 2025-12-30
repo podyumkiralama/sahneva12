ececd1754fd50835389ce3f3cfa0d722adc20a83
Murat <kasimrize@gmail.com>
2025-12-30 13:35:02 +0300

    Update ClarityProvider.jsx

--- components/ClarityProvider.jsx
+++ components/ClarityProvider.jsx
@@ -1,13 +1,7 @@
-"use client";
-
-import { useEffect } from "react";
-import clarity from "@microsoft/clarity";
-
-export default function ClarityProvider() {
-  useEffect(() => {
-    // Consent varsa buraya koşul koyarsın (şimdilik direkt başlatıyoruz)
-    clarity.start({ projectId: "utk0bu995w" });
-  }, []);
-
-  return null;
-}
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "utk0bu995w");
+</script>
